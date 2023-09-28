99e922398779a8fcc0ba909ad86aa53a8363a0ea
Abdul Alim <aa.abdulalim13@gmail.com>
2023-09-28 12:32:45 +0600

    update

---
+++ static/js/scripts.js
@@ -0,0 +1,42 @@
+// AOS
+AOS.init({
+  once: true, // Set the 'once' option to true to make all animations trigger only once
+});
+
+
+/// Swiper
+var swiper = new Swiper(".categorySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+const progressCircle = document.querySelector(".autoplay-progress svg");
+    const progressContent = document.querySelector(".autoplay-progress span");
+    var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      },
+      on: {
+        autoplayTimeLeft(s, time, progress) {
+          progressCircle.style.setProperty("--progress", 1 - progress);
+          progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+        }
+      }
+    });
--- static_root/js/scripts.js
+++ static_root/js/scripts.js
@@ -5,7 +5,7 @@ AOS.init({
 
 
 // Swiper
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".categorySwiper", {
   slidesPerView: 4,
   spaceBetween: 30,
   freeMode: true,
@@ -14,3 +14,29 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+
+
+const progressCircle = document.querySelector(".autoplay-progress svg");
+    const progressContent = document.querySelector(".autoplay-progress span");
+    var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      },
+      on: {
+        autoplayTimeLeft(s, time, progress) {
+          progressCircle.style.setProperty("--progress", 1 - progress);
+          progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+        }
+      }
+    });
