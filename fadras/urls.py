867f2aa7f617c4448c967419f2a7da5c24c80e85
Abdul Alim <aa.abdulalim13@gmail.com>
2023-10-08 10:22:37 +0600

    changed project name

--- fadras/urls.py
+++ fadras/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for arfen project.
+URL configuration for fadras project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
